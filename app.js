4a12fd66f7bc30773f77ed6fdd87ce4ff6e53876
SVS HARSHITH <svs.harshith>
2019-11-06 10:27:37 +0000

    added list page and upadated app.js acc

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 var express = require('express');  
 var bodyParser = require('body-parser');  
 var mongodb = require('mongodb'),  
-  MongoClient = mongodb.MongoClient;
+    MongoClient = mongodb.MongoClient;
 var assert = require('assert');  
 var util=require('util');
 var song;
-
+var unirest = require("unirest");
+var reqd = unirest("GET", "https://deezerdevs-deezer.p.rapidapi.com/search");
 var app = express();  
+ var result;
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());  
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
 
 
 app.get("/", function(req, res){
@@ -17,34 +23,31 @@ app.get("/", function(req, res){
 
 });
 
-
- app.get("/list",function(req,res){
+app.get("/list",function(req,res){
 	song=req.query.search;
-	 console.log(song);
-	 console.log(typeof song);
+	 //console.log(song);
+			reqd.query({
+			"q": "" + song+ ""
+		});
+
+		reqd.headers({
+			"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+			"x-rapidapi-key": "45eff3d17amsha881e5fec2254d6p1b4718jsn949ef4e06f56"
+		});
+		
+	    reqd.end(function (resd) {
+			 if (resd.error) throw new Error(resd.error);
+				result= resd.body;
+				console.log(result["data"][0]["artist"]["name"]);
+				// resd.render("list", {result: result});
+				res.render("list", {result: result});
+        });
+
+	  
 });
 
 
-var unirest = require("unirest");
 
-var req = unirest("GET", "https://deezerdevs-deezer.p.rapidapi.com/search");
-
-req.query({
-	"q": ""+song+""
-});
-
-req.headers({
-	"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-	"x-rapidapi-key": "45eff3d17amsha881e5fec2254d6p1b4718jsn949ef4e06f56"
-});
-
-
-req.end(function (res) {
-	if (res.error) throw new Error(res.error);
-    var result= res.body;
-//	console.log("ziga ziga");
-	console.log(result["data"]);
-});
 app.listen(3000,function(){
 	console.log("doof says yes")
 });
