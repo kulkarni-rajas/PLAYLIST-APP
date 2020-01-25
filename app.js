b5ad1212aaeb1d0fefea55d534fb03a2797991d8
v-rohan <rohanrao.dec11@gmail.com>
2020-01-25 15:08:55 +0530

    delete button

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var express     = require('express'),
 	app         = express(),  
 	flash       = require('connect-flash'),
 	result,song,obj2,empty=null; 
-const validator = require("validator")
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public')); 
@@ -20,12 +19,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"))
 app.use(flash());
 
-const port = 3012
-
-
 // connecting to monggose server
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/playlist_app",{ useNewUrlParser: true,useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost/playlist_app",{ useNewUrlParser: true });
 
 // passport authentication setup
 var UserSchema = new mongoose.Schema({
@@ -51,7 +47,6 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
    res.locals.currentUser = req.user;
-   //console.log(1,res.locals.currentUser)
    next();
 });
 
@@ -88,16 +83,14 @@ var Song = mongoose.model("Song", songSchema);
 
 
 app.get("/", function(req, res){
-	console.log("back1")
-	res.redirect("/back");
+        res.render("landing");
 
 });
 
 app.post("/list",function(req,res){
 	song=req.body.Search;
 	//song="light it up";
-	console.log(2,req.body)
-	console.log(3,req.body.Search);
+	console.log(req.body.Search);
 	// console.log(song);
 			reqd.query({
 			"q": "" +song+ ""
@@ -109,14 +102,10 @@ app.post("/list",function(req,res){
 		});
 		
 	    reqd.end(function (resd) {
-			 if (resd.error) 
-			 {throw new Error(resd.error);}
+			 if (resd.error) throw new Error(resd.error);
 				result= resd.body;
-				//console.log(result["data"][0]);
-				console.log(3,result)
-				const data = result["data"]
-				
-				data.forEach(function(song){
+			    //console.log(result["data"][0]);
+			    result["data"].forEach(function(song){
 					
 					Song.create({
 						   name:   song["title"],
@@ -153,7 +142,7 @@ app.get("/list_view",function(req,res){
 	app.get("/addplaylist/:id",isLoggedIn,function(req,res){
 		var idp=req.params.id;
 		//console.log(req.user.username);
-		//console.log(5,req.user)
+		
 	     PlaylistSC.find({}, function(err, songs){
 			if(err){
 				console.log("ERROR!");
@@ -168,7 +157,6 @@ app.get("/list_view",function(req,res){
 
 app.get("/newplaylist/:id",function(req,res){
 	var idp= req.params.id;
-
 	var name= req.query.plyname;
 	console.log(name);
 //	res.redirect("");
@@ -367,15 +355,11 @@ app.delete("/playlist/:id",function(req,res){
 	
 // });
 
-app.get("/back",(req,res)=>{
-	res.render("landing");
-})
-
 app.post("/back",function(req,res){
 	Song.deleteMany({}, function (err) {
 	  if (err) return handleError(err);
 	});
-	res.redirect("/back");
+	res.redirect("/");
 });
 
 app.post("/delete/:id",function(req,res){
@@ -389,51 +373,23 @@ app.post("/delete/:id",function(req,res){
 
 
 app.get("/register", function(req, res){
-   res.render("signup",{errors:undefined}); 
+   res.render("signup"); 
 });
 //handle sign up logic
 app.post("/register", function(req, res){
-
-
-	const{password,confirmPassword}=req.body
-    var errors =[]
-
-	if(validator.isAlphanumeric(password))
-	{
-		errors.push("Password should contain at least one special character")
-	}
-
-	if(password.length<=8)
-	{
-		errors.push("Password should contain at least 8 characters")
-	}
-
-// 	console.log(4,req.body)
-// 	const{password,confirmPassword}=req.body
-
-	if(password===confirmPassword)
-	{
-		
+	
     var newUser = new User({username: req.body.username});
     User.register(newUser, req.body.password, function(err, user){
         if(err){
-            
+            console.log(err);
             //return res.render("signup");
-			errors.push("Username already taken");
+			res.send("username already taken");
         }
          passport.authenticate("local")(req, res, function(){
 		//	 res.send("successfully registered");
-		console.log(errors)
-		res.render('signup',{errors:errors}) 
+              res.redirect("/"); 
         });
-	});
-}else{
-	errors.push("Password confirmation does not match password")
-	console.log(errors)
-	res.render('signup',{errors:errors}) 
-}
-
-		
+    });
 });
 
 // show login form
@@ -473,6 +429,6 @@ function registerLOG(req,res,next){
 		}
 }
 
-app.listen(port,function(){
-	console.log("The app is active on ",port);
+app.listen(3000,function(){
+	console.log("doof says yes");
 });
